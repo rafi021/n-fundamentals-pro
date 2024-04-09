7d501a6831b9f3f5c6c5a129ad7b12979d1fa1c3
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2024-04-09 16:25:39 +0600

    test mock service

--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -2,36 +2,36 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class SongsService {
-    // local db
-    // local query
+  // local db
+  // local query
 
-    private readonly songs = [];
+  private readonly songs = [];
 
-    create(song){
-        this.songs.push(song);
-        return this.songs;
-    }
+  create(song) {
+    this.songs.push(song);
+    return this.songs;
+  }
 
-    findAll(){
-        // fetch the songs from the database
-        // Erros comes while fetching the data from the database
-        throw new Error ("Error in DB while fetching records");
-        return this.songs;
-    }
+  findAll() {
+    // fetch the songs from the database
+    // Erros comes while fetching the data from the database
+    // throw new Error ("Error in DB while fetching records");
+    return this.songs;
+  }
 
-    findOne(id: number){
-        return this.songs.find(song => song.id === id);
-    }
+  findOne(id: number) {
+    return this.songs.find((song) => song.id === id);
+  }
 
-    update(id: number, title: string, artist: string, album: string){
-        const song = this.songs.find(song => song.id === id);
-        song.title = title;
-        song.artist = artist;
-        song.album = album;
-        return song;
-    }
-    
-    delete(id: number){
-        return this.songs.filter(song => song.id!== id);
-    }
+  update(id: number, title: string, artist: string, album: string) {
+    const song = this.songs.find((song) => song.id === id);
+    song.title = title;
+    song.artist = artist;
+    song.album = album;
+    return song;
+  }
+
+  delete(id: number) {
+    return this.songs.filter((song) => song.id !== id);
+  }
 }
