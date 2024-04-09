e5cec4766c61393dd605c97f7f660fe89ecf1376
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2024-04-09 22:47:56 +0600

    connecting db and testing db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,9 +10,23 @@ import { SongsModule } from './songs/songs.module';
 import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
 import { SongsController } from './songs/songs.controller';
 import { DevConfigService } from './common/providers/DevConfigService';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [SongsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'mediumclone',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    SongsModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
@@ -23,6 +37,10 @@ import { DevConfigService } from './common/providers/DevConfigService';
   ],
 })
 export class AppModule implements NestModule {
+  constructor(dataSource: DataSource) {
+    console.log('dbName: ' + dataSource.driver.database);
+  }
+
   configure(consumer: MiddlewareConsumer) {
     //  consumer.apply(LoggerMiddleware).forRoutes('songs'); // option :1
     //  consumer.apply(LoggerMiddleware).forRoutes({path: 'songs', method: RequestMethod.POST }); // option :2;
