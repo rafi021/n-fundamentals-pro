47af81192a0cacafe5f6330eca493f32d8055265
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2024-04-09 22:11:59 +0600

    Class Based Service Provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,31 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
 import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
 import { SongsController } from './songs/songs.controller';
+import { DevConfigService } from './common/providers/DevConfigService';
 
 @Module({
   imports: [SongsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: DevConfigService,
+      useClass: DevConfigService,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-  //  consumer.apply(LoggerMiddleware).forRoutes('songs'); // option :1 
-  //  consumer.apply(LoggerMiddleware).forRoutes({path: 'songs', method: RequestMethod.POST }); // option :2;
-  consumer.apply(LoggerMiddleware).forRoutes(SongsController);  // option :3;
-  
-}  
+    //  consumer.apply(LoggerMiddleware).forRoutes('songs'); // option :1
+    //  consumer.apply(LoggerMiddleware).forRoutes({path: 'songs', method: RequestMethod.POST }); // option :2;
+    consumer.apply(LoggerMiddleware).forRoutes(SongsController); // option :3;
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { DevConfigService } from './common/providers/DevConfigService';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly devconfigService: DevConfigService) {}
+
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!' + this.devconfigService.getDBHOST();
   }
 }
